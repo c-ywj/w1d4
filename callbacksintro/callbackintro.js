96a35cd8f37a0baf461e6c788faed8e33993d22f
Carson Yan <cybc00@hotmail.com>
2017-02-09 16:36:37 +0000

    refactored findWaldo function with forEach method

--- callbacksintro/callbackintro.js
+++ callbacksintro/callbackintro.js
@@ -1,14 +1,23 @@
 // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  arr.forEach(found);
 }
 
-function actionWhenFound(index) {
+
+
+function actionWhenFound(value, index) {
+  if(value === "Waldo") {
   console.log("Found Waldo at index: " + index);
+  }
 }
 
 
